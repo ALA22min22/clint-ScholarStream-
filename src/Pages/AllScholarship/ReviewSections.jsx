7d39884b78c44d4f13a904d941909099b0dcf2bc
alamin22 <alaminhossan4000@gmail.com>
2025-12-23 13:57:13 +0600

    make home page top scholership card recent added scholership and modify review sections

--- src/Pages/AllScholarship/ReviewSections.jsx
+++ src/Pages/AllScholarship/ReviewSections.jsx
@@ -13,39 +13,99 @@ const ReviewSections = ({ scholarship }) => {
         }
     })
 
-    if(isLoading){
-      return  <Loading></Loading>
+    if (isLoading) {
+        return <Loading></Loading>
     }
-    
-    if(isError){
+
+    if (isError) {
         return <div>Error is here in the data......</div>
     }
     refetch();
 
     return (
         <div >
-                <h3 className="text-5xl text-black font-bold text-center my-10">All Reviews</h3>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-6xl mx-auto '>
+            <h3 className="text-5xl text-black font-bold text-center my-10">All Reviews</h3>
+
+            <div className='max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    review.map(data => <div className="card bg-base-100 image-full   shadow-sm">
-                <figure>
-                    <img
-                        src={data.reviewerImage}
-                        alt=""  className='w-full  '/>
-                </figure>
-                <div className="card-body text-white">
-                    <h2 className="card-title">{data.reviewerName}</h2>
-                    
-                    <div className="card-actions justify-end">
-                        <p>{data.date}</p>
-                        <button className="btn btn-primary"> Retting: {data.rating}</button>
-                    </div>
-                    <p className='break-words'> <span className='text-2xl font-semibold'>Comment:</span> {data.comment}</p>
-                    
-                </div>
-            </div>)
+                    review.map(data =>
+                        <div key={data._id}
+                            class="flex flex-col gap-2 dark:text-white max-w-md w-full bg-white dark:bg-neutral-900 p-5 rounded-md mt-8 shadow-md hover:scale-105 hover:duration-150 duration-150 " style={{backgroundImage: `url(${data.reviewerImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+                        >
+                            <div class="flex flex-row justify-between w-full">
+                                <div class="flex flex-row justify-between w-full items-center">
+                                    <p class="text-xl w-5/12 break-word">{data.reviewerName}</p>
+                                    <p class="text-xs">{data.date}</p>
+                                </div>
+                            </div>
+                            <div class="flex flex-row justify-between w-full">
+
+                                <div class="text-xs">
+                                    <div class="flex flex-row gap-1 items-center">
+                                       <span className='text-sm'> Retting: {data.rating}</span>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="h-4 w-4 text-yellow-400"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                                            ></path>
+                                        </svg>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="h-4 w-4 text-yellow-400"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                                            ></path>
+                                        </svg>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="h-4 w-4 text-yellow-400"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                                            ></path>
+                                        </svg>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="h-4 w-4 text-yellow-400"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                                            ></path>
+                                        </svg>
+
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            class="h-4 w-4 text-yellow-200"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                d="M9.049 2.927c.3-.916 1.603-.916 1.902 0l1.286 3.953a1.5 1.5 0 001.421 1.033h4.171c.949 0 1.341 1.154.577 1.715l-3.38 2.458a1.5 1.5 0 00-.54 1.659l1.286 3.953c.3.916-.757 1.67-1.539 1.145l-3.38-2.458a1.5 1.5 0 00-1.76 0l-3.38 2.458c-.782.525-1.838-.229-1.539-1.145l1.286-3.953a1.5 1.5 0 00-.54-1.659l-3.38-2.458c-.764-.561-.372-1.715.577-1.715h4.171a1.5 1.5 0 001.421-1.033l1.286-3.953z"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="text-sm">
+                                {data.comment}
+                            </div>
+                        </div>)
                 }
             </div>
+
+
         </div>
     );
 };
